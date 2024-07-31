0872d7e6d6d88d4da46bfa927e645e0aebbcf8d8
Alex S <asorescu@imsa.edu>
2024-07-31 15:04:29 -0500

    first commit
    first commit

---
+++ src/components/nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css_pages/navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/projects">Projects</Link></li>
+                <li><Link to="/notes">Notes</Link></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
